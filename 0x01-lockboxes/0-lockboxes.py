6dbec4b4e9eec2178b430cfcb398a505ac8aa181
Michafolab <TOPSONMICH1@GMAIL.COM>
2024-01-14 21:40:33 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """module to unlock all the boxes
+    Args:
+        boxes - boxes to unlock
+    """
+    total_box = len(boxes)
+    index = 0
+    setofkeys = [0]
+    counter = 0
+
+    while index < total_box:
+        set_key = setofkeys[index]
+        for key in boxes[set_key]:
+            if 0 < key < total_box and key not in setofkey:
+                setofkey.apeend(key)
+                counter += 1
+        index += 1
+    return counter == total_box - 1
